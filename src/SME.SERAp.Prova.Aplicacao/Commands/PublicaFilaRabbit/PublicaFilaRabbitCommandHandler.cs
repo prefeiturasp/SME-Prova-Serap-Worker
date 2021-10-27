9c01a4a7158d9b8cd5928adc21931569e3235c66
Luiz Henrique Araújo <luizhbd@gmail.com>
2021-10-27 18:23:08 -0300

    Update PublicaFilaRabbitCommandHandler.cs

--- src/SME.SERAp.Prova.Aplicacao/Commands/PublicaFilaRabbit/PublicaFilaRabbitCommandHandler.cs
+++ src/SME.SERAp.Prova.Aplicacao/Commands/PublicaFilaRabbit/PublicaFilaRabbitCommandHandler.cs
@@ -12,13 +12,13 @@ namespace SME.SERAp.Prova.Aplicacao.Commands.FilaWorker
 {
     public class PublicaFilaRabbitCommandHandler : IRequestHandler<PublicaFilaRabbitCommand, bool>
     {
-       private readonly ConnectionFactory factory;
-        
+        private readonly ConnectionFactory factory;
+
         public PublicaFilaRabbitCommandHandler(ConnectionFactory factory)
         {
             this.factory = factory ?? throw new ArgumentNullException(nameof(factory));
         }
-        
+
         public Task<bool> Handle(PublicaFilaRabbitCommand request, CancellationToken cancellationToken)
         {
             //TODO: Implementar Polly!
@@ -26,21 +26,23 @@ namespace SME.SERAp.Prova.Aplicacao.Commands.FilaWorker
             {
                 var conexaoRabbit = factory.CreateConnection();
                 var model = conexaoRabbit.CreateModel();
-                
+
                 var mensagem = new MensagemRabbit(request.Mensagem, Guid.NewGuid());
-        
+
                 var mensagemJson = JsonSerializer.Serialize(mensagem);
                 var body = Encoding.UTF8.GetBytes(mensagemJson);
-        
+
                 var props = model.CreateBasicProperties();
                 props.Persistent = true;
-        
+
                 model.BasicPublish(ExchangeRabbit.SerapEstudante, request.NomeRota, props, body);
-        
+
                 return Task.FromResult(true);
             }
             catch (Exception ex)
             {
+                SentrySdk.AddBreadcrumb($"Erros: PublicaFilaRabbitCommand", null, null, null, BreadcrumbLevel.Error);
+                SentrySdk.CaptureMessage($"Worker Serap: Rota -> {request.NomeRota} Fila -> {request.NomeFila}", SentryLevel.Error);
                 SentrySdk.CaptureException(ex);
                 return Task.FromResult(false);
             }

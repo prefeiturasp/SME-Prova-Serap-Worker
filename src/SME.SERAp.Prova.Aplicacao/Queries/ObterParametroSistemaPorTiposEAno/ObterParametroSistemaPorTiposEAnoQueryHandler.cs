bf137c9d8d9df7f9b45b728703953ad58ab00180
Luiz Henrique Araújo <luizhbd@gmail.com>
2021-12-15 09:58:29 -0300

    Ajuste para adicionar o cache

--- src/SME.SERAp.Prova.Aplicacao/Queries/ObterParametroSistemaPorTiposEAno/ObterParametroSistemaPorTiposEAnoQueryHandler.cs
+++ src/SME.SERAp.Prova.Aplicacao/Queries/ObterParametroSistemaPorTiposEAno/ObterParametroSistemaPorTiposEAnoQueryHandler.cs
@@ -11,14 +11,16 @@ namespace SME.SERAp.Prova.Aplicacao
     public class ObterParametroSistemaPorTiposEAnoQueryHandler : IRequestHandler<ObterParametroSistemaPorTiposEAnoQuery, IEnumerable<ParametroSistema>>
     {
         private readonly IRepositorioParametroSistema repositorioParametroSistema;
+        private readonly IRepositorioCache repositorioCache;
 
-        public ObterParametroSistemaPorTiposEAnoQueryHandler(IRepositorioParametroSistema repositorioParametroSistema)
+        public ObterParametroSistemaPorTiposEAnoQueryHandler(IRepositorioParametroSistema repositorioParametroSistema, IRepositorioCache repositorioCache)
         {
             this.repositorioParametroSistema = repositorioParametroSistema ?? throw new System.ArgumentNullException(nameof(repositorioParametroSistema));
+            this.repositorioCache = repositorioCache ?? throw new System.ArgumentNullException(nameof(repositorioCache));
         }
         public async Task<IEnumerable<ParametroSistema>> Handle(ObterParametroSistemaPorTiposEAnoQuery request, CancellationToken cancellationToken)
         {
-            var parametrosDoSistema = await repositorioParametroSistema.ObterTudoAsync();
+            var parametrosDoSistema = await repositorioCache.ObterRedisAsync("parametros", async () => await repositorioParametroSistema.ObterTudoAsync(), 1440);
 
             if (parametrosDoSistema != null && parametrosDoSistema.Any())
                 return parametrosDoSistema.Where(a => a.Ano == request.Ano && request.Tipos.Contains((int)a.Tipo));

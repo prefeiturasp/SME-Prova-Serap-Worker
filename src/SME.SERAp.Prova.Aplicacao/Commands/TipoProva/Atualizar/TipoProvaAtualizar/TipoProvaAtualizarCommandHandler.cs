b4363cbb28b023632d02d7cc85b2d0328ae9cf43
krisamcom <68442245+krisamcom@users.noreply.github.com>
2022-01-26 17:06:25 -0300

    story:51188 - atualizar tipo prova

---
+++ src/SME.SERAp.Prova.Aplicacao/Commands/TipoProva/Atualizar/TipoProvaAtualizar/TipoProvaAtualizarCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using SME.SERAp.Prova.Dados;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SERAp.Prova.Aplicacao
+{
+    public class TipoProvaAtualizarCommandHandler : IRequestHandler<TipoProvaAtualizarCommand, bool>
+    {
+
+        private readonly IRepositorioTipoProva repositorioTipoProva;
+
+        public TipoProvaAtualizarCommandHandler(IRepositorioTipoProva repositorioTipoProva)
+        {
+            this.repositorioTipoProva = repositorioTipoProva ?? throw new System.ArgumentNullException(nameof(repositorioTipoProva));
+        }
+
+        public async Task<bool> Handle(TipoProvaAtualizarCommand request, CancellationToken cancellationToken)
+        {
+            await repositorioTipoProva.UpdateAsync(request.TipoProva);
+            return true;
+        }
+    }
+}

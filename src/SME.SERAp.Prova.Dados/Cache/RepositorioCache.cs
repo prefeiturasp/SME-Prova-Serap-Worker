8918e7d53a09830e131e8a9d22abcb8a69128ae9
Juliano de Campos Martins <juliano_campos_92@hotmail.com>
2022-04-08 18:17:26 -0400

    removendo AppInsights e ajustando cache

--- src/SME.SERAp.Prova.Dados/Cache/RepositorioCache.cs
+++ src/SME.SERAp.Prova.Dados/Cache/RepositorioCache.cs
@@ -1,72 +1,87 @@
 ﻿using MessagePack;
-using Microsoft.Extensions.Caching.Distributed;
-using Microsoft.Extensions.Caching.Memory;
 using SME.SERAp.Prova.Infra.Interfaces;
-using SME.SERAp.Prova.Infra.Utils;
+using StackExchange.Redis;
 using System;
-using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace SME.SERAp.Prova.Dados.Cache
 {
     public class RepositorioCache : IRepositorioCache
     {
-
         private readonly IServicoLog servicoLog;
-        private readonly IDistributedCache distributedCache;
-
-        public RepositorioCache(IServicoLog servicoLog, IDistributedCache distributedCache)
+        private readonly IDatabase database;
+        public RepositorioCache(IServicoLog servicoLog, IConnectionMultiplexer connectionMultiplexer)
         {
             this.servicoLog = servicoLog ?? throw new ArgumentNullException(nameof(servicoLog));
-            this.distributedCache = distributedCache ?? throw new ArgumentNullException(nameof(distributedCache));
+            if (connectionMultiplexer == null) throw new ArgumentNullException(nameof(connectionMultiplexer));
+            this.database = connectionMultiplexer.GetDatabase();
+        }
+
+        public async Task SalvarRedisAsync(string nomeChave, object valor, int minutosParaExpirar = 720)
+        {
+            try
+            {
+                await database.StringSetAsync(nomeChave, MessagePackSerializer.Serialize(valor), TimeSpan.FromMinutes(minutosParaExpirar));
+            }
+            catch (Exception ex)
+            {
+                servicoLog.Registrar(ex);
+            }
+        }
+
+        public async Task RemoverRedisAsync(string nomeChave)
+        {
+            try
+            {
+                await database.KeyDeleteAsync(nomeChave);
+            }
+            catch (Exception ex)
+            {
+                servicoLog.Registrar(ex);
+            }
         }
 
         public async Task<T> ObterRedisAsync<T>(string nomeChave, Func<Task<T>> buscarDados, int minutosParaExpirar = 720)
         {
             try
             {
-                var byteCache = await distributedCache.GetAsync(nomeChave);
+                byte[] byteCache = await database.StringGetAsync(nomeChave);
+
                 if (byteCache != null)
                 {
                     return MessagePackSerializer.Deserialize<T>(byteCache);
                 }
 
                 var dados = await buscarDados();
-
                 await SalvarRedisAsync(nomeChave, dados, minutosParaExpirar);
 
                 return dados;
-
             }
             catch (Exception ex)
             {
                 servicoLog.Registrar(ex);
-
-                return default;
             }
-        }        
 
-        public async Task RemoverRedisAsync(string nomeChave)
+            return default;
+        }
+
+        public async Task<T> ObterRedisAsync<T>(string nomeChave)
         {
             try
             {
-                await distributedCache.RemoveAsync(nomeChave);
+                byte[] byteCache = await database.StringGetAsync(nomeChave);
+
+                if (byteCache != null)
+                {
+                    return MessagePackSerializer.Deserialize<T>(byteCache);
+                }
             }
             catch (Exception ex)
             {
                 servicoLog.Registrar(ex);
             }
-        }
-        public async Task SalvarRedisAsync(string nomeChave, object valor, int minutosParaExpirar = 720)
-        {
-            var inicioOperacao = DateTime.UtcNow;
-            var timer = System.Diagnostics.Stopwatch.StartNew();
-            {
-                await distributedCache.SetAsync(nomeChave, MessagePackSerializer.Serialize(valor), new DistributedCacheEntryOptions()
-                                                .SetAbsoluteExpiration(TimeSpan.FromMinutes(minutosParaExpirar)));
 
-                timer.Stop();
-            }
+            return default;
         }
     }
 }

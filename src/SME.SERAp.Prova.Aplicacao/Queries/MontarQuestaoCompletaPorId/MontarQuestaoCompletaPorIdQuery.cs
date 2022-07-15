9adbfec4683eb841e918117cac0aa65a64ca47ad
Juliano Martins <juliano_campos_92@hotmail.com>
2022-07-15 19:55:07 -0400

    adequação questão completa por id legado

---
+++ src/SME.SERAp.Prova.Aplicacao/Queries/MontarQuestaoCompletaPorId/MontarQuestaoCompletaPorIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using SME.SERAp.Prova.Infra;
+
+namespace SME.SERAp.Prova.Aplicacao
+{
+    public class MontarQuestaoCompletaPorIdQuery : IRequest<QuestaoCompletaDto>
+    {
+        public MontarQuestaoCompletaPorIdQuery(long questaoId)
+        {
+            QuestaoId = questaoId;
+        }
+
+        public long QuestaoId { get; set; }
+    }
+}

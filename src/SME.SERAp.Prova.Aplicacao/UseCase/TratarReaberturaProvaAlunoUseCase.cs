1ef4bc33d7ec5cba19810fafab18ba46be388ec3
Caique Siqueira <latorrecaique@gmail.com>
2022-09-12 12:16:16 -0300

    AB#70995

--- src/SME.SERAp.Prova.Aplicacao/UseCase/TratarReaberturaProvaAlunoUseCase.cs
+++ src/SME.SERAp.Prova.Aplicacao/UseCase/TratarReaberturaProvaAlunoUseCase.cs
@@ -1,13 +1,31 @@
-﻿using System;
+﻿using MediatR;
+using SME.SERAp.Prova.Infra;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SME.SERAp.Prova.Aplicacao.UseCase.PersistenciaSerap
 {
-   public class TratarReaberturaProvaAlunoUseCase
+    public class TratarReaberturaProvaAlunoUseCase : ITratarReaberturaProvaAlunoUseCase
     {
-        // Incluir log 
-        // chamar a fila 
-        //IncluirProvaAlunoReaberturaCommand
+        private readonly IMediator mediator;
+
+        public TratarReaberturaProvaAlunoUseCase(IMediator mediator)
+        {
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+
+        public async Task<bool> Executar(MensagemRabbit mensagemRabbit)
+        {
+            return true;
+            //* delete from prova_aluno where aluno_ra and prova_id
+            //* incluir o log de quem fez a solicitação
+            // * chama fila do acompanhamento para atualizar o registro
+            // Incluir log 
+            // chamar a fila 
+            //IncluirProvaAlunoReaberturaCommand
+        }
     }
 }

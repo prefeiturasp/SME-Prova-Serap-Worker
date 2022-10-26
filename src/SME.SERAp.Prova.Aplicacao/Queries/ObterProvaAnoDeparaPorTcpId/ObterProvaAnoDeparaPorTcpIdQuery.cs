083ba598755227917ad6a9bf544a33eed11bdf01
Juliano Martins <juliano_campos_92@hotmail.com>
2022-10-26 16:26:34 -0400

    Bug 70597: Ano indevido vinculado a prova.

---
+++ src/SME.SERAp.Prova.Aplicacao/Queries/ObterProvaAnoDeparaPorTcpId/ObterProvaAnoDeparaPorTcpIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using SME.SERAp.Prova.Infra.Dtos;
+using System.Collections.Generic;
+
+namespace SME.SERAp.Prova.Aplicacao
+{
+    public class ObterProvaAnoDeparaPorTcpIdQuery : IRequest<IEnumerable<TipoCurriculoPeriodoAnoDto>>
+    {
+        public ObterProvaAnoDeparaPorTcpIdQuery(int[] tcpIds)
+        {
+            TcpIds = tcpIds;
+        }
+
+        public int[] TcpIds { get; set; }
+    }
+}

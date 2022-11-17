d27b258bf5825f9d1526c4fc774fc49fb54cc58f
Juliano Martins <juliano_campos_92@hotmail.com>
2022-11-17 15:24:38 -0400

    implementação

---
+++ src/SME.SERAp.Prova.Aplicacao/Queries/ObterProvaOrigemCaderno/ObterProvaOrigemCadernoQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace SME.SERAp.Prova.Aplicacao
+{
+    public class ObterProvaOrigemCadernoQuery : IRequest<long?>
+    {
+        public ObterProvaOrigemCadernoQuery(long provaId)
+        {
+            ProvaId = provaId;
+        }
+
+        public long ProvaId { get; set; }
+    }
+}

8e6824d144fb26bb8895dac45b2ad94f3851311b
Fernando Groeler <fernando.groeler@gmail.com>
2023-02-03 16:45:59 -0400

    Adequada declaração de filas de deadletter adicionando parâmetros configuraveis.

--- src/SME.SERAp.Prova.Infra/Fila/ComandoRabbit.cs
+++ src/SME.SERAp.Prova.Infra/Fila/ComandoRabbit.cs
@@ -4,13 +4,30 @@ namespace SME.SERAp.Prova.Infra
 {
     public class ComandoRabbit
     {
-        public ComandoRabbit(string nomeProcesso, Type tipoCasoUso)
+        public ComandoRabbit(string nomeProcesso, Type tipoCasoUso, ulong quantidadeReprocessamentoDeadLetter = 3,
+            int ttl = 10 * 60 * 100)
         {
             NomeProcesso = nomeProcesso;
             TipoCasoUso = tipoCasoUso;
+            QuantidadeReprocessamentoDeadLetter = quantidadeReprocessamentoDeadLetter;
+            Ttl = ttl;
         }
+        
+        public ComandoRabbit(string nomeProcesso, Type tipoCasoUso, bool modeLazy) : this(nomeProcesso, tipoCasoUso)
+        {
+            ModeLazy = modeLazy;
+        }
+        
+        public ComandoRabbit(string nomeProcesso, Type tipoCasoUso, bool modeLazy, ulong quantidadeReprocessamentoDeadLetter,
+            int ttl) : this (nomeProcesso, tipoCasoUso, quantidadeReprocessamentoDeadLetter, ttl)
+        {
+            ModeLazy = modeLazy;
+        }        
 
-        public string NomeProcesso { get; private set; }
-        public Type TipoCasoUso { get; private set; }
+        public string NomeProcesso { get; }
+        public Type TipoCasoUso { get; }
+        public ulong QuantidadeReprocessamentoDeadLetter { get; } = 3;
+        public int Ttl { get; } = 10 * 60 * 100;
+        public bool ModeLazy { get; }
     }
 }
--- src/SME.SERAp.Prova.Infra/Services/ServicoLog.cs
+++ src/SME.SERAp.Prova.Infra/Services/ServicoLog.cs
@@ -1,5 +1,4 @@
-﻿
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using RabbitMQ.Client;
 using SME.SERAp.Prova.Dominio;
 using SME.SERAp.Prova.Dominio.Enums;
@@ -14,6 +13,7 @@ namespace SME.SERAp.Prova.Infra.Services
     {
         private readonly IServicoTelemetria servicoTelemetria;
         private readonly RabbitLogOptions configuracaoRabbitOptions;
+
         public ServicoLog(IServicoTelemetria servicoTelemetria, RabbitLogOptions configuracaoRabbitOptions)
         {
             this.servicoTelemetria = servicoTelemetria ?? throw new ArgumentNullException(nameof(servicoTelemetria));
@@ -22,37 +22,37 @@ namespace SME.SERAp.Prova.Infra.Services
 
         public void Registrar(Exception ex)
         {
-            LogMensagem logMensagem = new LogMensagem("Exception --- ", LogNivel.Critico, ex.Message, ex.StackTrace);
+            var logMensagem = new LogMensagem("Exception --- ", LogNivel.Critico, ex.Message, ex.StackTrace);
             Registrar(logMensagem);
         }
 
         public void Registrar(LogNivel nivel, string erro, string observacoes, string stackTrace)
         {
-            LogMensagem logMensagem = new LogMensagem(erro, nivel, observacoes, stackTrace);
+            var logMensagem = new LogMensagem(erro, nivel, observacoes, stackTrace);
             Registrar(logMensagem);
         }
 
         public void Registrar(LogNivel nivel, string mensagem)
         {
-            LogMensagem logMensagem = new LogMensagem(mensagem, nivel, "");
+            var logMensagem = new LogMensagem(mensagem, nivel, "");
             Registrar(logMensagem);
         }
 
         public void Registrar(string mensagem, Exception ex)
         {
-            LogMensagem logMensagem = new LogMensagem(mensagem, LogNivel.Critico, ex.Message, ex.StackTrace);
-
+            var logMensagem = new LogMensagem(mensagem, LogNivel.Critico, ex.Message, ex.StackTrace);
             Registrar(logMensagem);
         }
+        
         private void Registrar(LogMensagem log)
         {
-                var mensagem = JsonConvert.SerializeObject(log, new JsonSerializerSettings
-                {
-                    NullValueHandling = NullValueHandling.Ignore
-                });
+            var mensagem = JsonConvert.SerializeObject(log, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            });
 
-                var body = Encoding.UTF8.GetBytes(mensagem);
-                servicoTelemetria.Registrar(() => PublicarMensagem(body), "RabbitMQ", "Salvar Log Via Rabbit", RotasRabbit.RotaLogs);
+            var body = Encoding.UTF8.GetBytes(mensagem);
+            servicoTelemetria.Registrar(() => PublicarMensagem(body), "RabbitMQ", "Salvar Log Via Rabbit", RotasRabbit.RotaLogs);
         }
 
         private void PublicarMensagem(byte[] body)
@@ -65,15 +65,11 @@ namespace SME.SERAp.Prova.Infra.Services
                 VirtualHost = configuracaoRabbitOptions.VirtualHost
             };
 
-            using (var conexaoRabbit = factory.CreateConnection())
-            {
-                using (IModel _channel = conexaoRabbit.CreateModel())
-                {
-                    var props = _channel.CreateBasicProperties();
-                    props.Persistent = true;
-                    _channel.BasicPublish(ExchangeRabbit.Logs, RotasRabbit.RotaLogs, props, body);
-                }
-            }
+            using var conexaoRabbit = factory.CreateConnection();
+            using var channel = conexaoRabbit.CreateModel();
+            var props = channel.CreateBasicProperties();
+            props.Persistent = true;
+            channel.BasicPublish(ExchangeRabbit.Logs, RotasRabbit.RotaLogs, props, body);
         }
     }
 }

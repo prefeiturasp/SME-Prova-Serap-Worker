b7a7e6e693a1cbe78b42c159943e7a298cffcb8c
Juliano Martins <juliano_campos_92@hotmail.com>
2023-02-09 18:14:01 -0400

    AB#83209: [TAI] - Erro ao sincronizar a atualização de uma prova

---
+++ src/SME.SERAp.Prova.Aplicacao/Commands/ProvaRemoverQuestoesTriPorId/ProvaRemoverQuestoesTriPorIdCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using SME.SERAp.Prova.Dados;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SERAp.Prova.Aplicacao
+{
+    public class ProvaRemoverQuestoesTriPorIdCommandHandler : IRequestHandler<ProvaRemoverQuestoesTriPorIdCommand, bool>
+    {
+        private readonly IRepositorioQuestaoTri repositorioQuestaoTri;
+
+        public ProvaRemoverQuestoesTriPorIdCommandHandler(IRepositorioQuestaoTri repositorioQuestaoTri)
+        {
+            this.repositorioQuestaoTri = repositorioQuestaoTri ?? throw new ArgumentNullException(nameof(repositorioQuestaoTri));
+        }
+
+        public async Task<bool> Handle(ProvaRemoverQuestoesTriPorIdCommand request, CancellationToken cancellationToken)
+        {
+            return await repositorioQuestaoTri.RemoverPorProvaIdAsync(request.Id);
+        }
+    }
+}

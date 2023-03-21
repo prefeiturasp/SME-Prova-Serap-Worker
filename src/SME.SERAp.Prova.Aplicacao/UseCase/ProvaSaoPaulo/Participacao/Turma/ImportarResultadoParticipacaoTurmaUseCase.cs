a292b2bd5d2a4cf164c31ac859376313e56a740a
Caíque Siqueira <latorrecaique@gmail.com>
2023-03-21 19:29:21 -0300

    Criar Estrutura do processo AB#84532

---
+++ src/SME.SERAp.Prova.Aplicacao/UseCase/ProvaSaoPaulo/Participacao/Turma/ImportarResultadoParticipacaoTurmaUseCase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.SERAp.Prova.Aplicacao
+{
+    public class ImportarResultadoParticipacaoTurmaUseCase
+    {
+    }
+}

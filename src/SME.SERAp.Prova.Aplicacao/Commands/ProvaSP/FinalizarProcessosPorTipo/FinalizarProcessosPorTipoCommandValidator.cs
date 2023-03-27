b414b8da9cb15c922d5bda342ad42f180203cd0f
Fernando Groeler <fernando.groeler@gmail.com>
2023-03-27 17:14:38 -0400

    Cherry-pick liberação

---
+++ src/SME.SERAp.Prova.Aplicacao/Commands/ProvaSP/FinalizarProcessosPorTipo/FinalizarProcessosPorTipoCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace SME.SERAp.Prova.Aplicacao
+{
+    public class FinalizarProcessosPorTipoCommandValidator : AbstractValidator<FinalizarProcessosPorTipoCommand>
+    {
+        public FinalizarProcessosPorTipoCommandValidator()
+        {
+            RuleFor(x => x.TipoResultadoProcesso).IsInEnum()
+                .WithMessage("TipoResultadoProcesso inválido");
+        }
+    }
+}

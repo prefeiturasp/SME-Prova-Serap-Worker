de5b3a70912a7b53c6559cfea8f1c29c3cc6615e
fernando.groeler <fernando.groeler@gmail.com>
2023-08-07 07:59:03 -0400

    Implementada gravação dos critérios do aluno.

--- src/SME.SERAp.Prova.Dados/Repositorios/ProvaSP/RepositorioResultadoAluno.cs
+++ src/SME.SERAp.Prova.Dados/Repositorios/ProvaSP/RepositorioResultadoAluno.cs
@@ -11,30 +11,35 @@ namespace SME.SERAp.Prova.Dados.Repositorios
 {
     public class RepositorioResultadoAluno : RepositorioProvaSpBase, IRepositorioResultadoAluno
     {
-
         public RepositorioResultadoAluno(ConnectionStringOptions connectionStringOptions) : base(connectionStringOptions) { }
 
         public async Task<ResultadoAluno> ObterProficienciaAluno(string edicao, string alunoMatricula, long areaConhecimentoId)
         {
-            var query = $@"SELECT  Edicao
-                                  ,AreaConhecimentoID
-                                  ,uad_sigla
-                                  ,esc_codigo
-                                  ,AnoEscolar
-                                  ,tur_codigo
-                                  ,tur_id
-                                  ,alu_matricula
-                                  ,alu_nome
-                                  ,NivelProficienciaID
-                                  ,Valor
-                              FROM ResultadoAluno with (nolock)
-							where alu_matricula = @alunoMatricula
-								and AreaConhecimentoID = @areaConhecimentoId
-                                and Edicao = @edicao
-						order by Edicao desc";
+            const string query = @"SELECT Edicao
+                                        ,AreaConhecimentoID
+                                        ,uad_sigla
+                                        ,esc_codigo
+                                        ,AnoEscolar
+                                        ,tur_codigo
+                                        ,tur_id
+                                        ,alu_matricula
+                                        ,alu_nome
+                                        ,NivelProficienciaID
+                                        ,Valor
+                                        ,REDQ1
+                                        ,REDQ2
+                                        ,REDQ3
+                                        ,REDQ4
+                                        ,REDQ5
+                                    FROM ResultadoAluno with (nolock)
+                                    where alu_matricula = @alunoMatricula
+                                    and AreaConhecimentoID = @areaConhecimentoId
+                                    and Edicao = @edicao
+                                    order by Edicao desc";
 
             using var conn = ObterConexaoProvaSp();
-            return await conn.QueryFirstOrDefaultAsync<ResultadoAluno>(query, new { alunoMatricula, areaConhecimentoId, edicao });
+            return await conn.QueryFirstOrDefaultAsync<ResultadoAluno>(query,
+                new { alunoMatricula, areaConhecimentoId, edicao });
         }
 
         public async Task<long> IncluirAsync(ResultadoAluno resultado)
@@ -42,38 +47,47 @@ namespace SME.SERAp.Prova.Dados.Repositorios
             using var conn = ObterConexaoProvaSp();
             try
             {
-                var query = $@"INSERT INTO ResultadoAluno
-                             (Edicao
-                             ,AreaConhecimentoID
-                      	     ,uad_sigla
-                             ,esc_codigo
-                             ,AnoEscolar
-                             ,tur_codigo
-                             ,tur_id
-                             ,alu_matricula
-                             ,alu_nome
-                             ,NivelProficienciaID
-                             ,Valor)
-                              VALUES
-                             ( @Edicao  
-                             , @AreaConhecimentoID
-                             , @uad_sigla 
-                             , REPLICATE('0', 6 - LEN(@esc_codigo)) + RTrim(@esc_codigo)
-                             , @AnoEscolar
-                             , @tur_codigo
-                             , @tur_id
-                             , @alu_matricula
-                             , @alu_nome
-                             , @NivelProficienciaID
-                             , @Valor)";
+                const string query = @"INSERT INTO ResultadoAluno
+                                        (Edicao
+                                        ,AreaConhecimentoID
+                                        ,uad_sigla
+                                        ,esc_codigo
+                                        ,AnoEscolar
+                                        ,tur_codigo
+                                        ,tur_id
+                                        ,alu_matricula
+                                        ,alu_nome
+                                        ,NivelProficienciaID
+                                        ,Valor
+                                        ,REDQ1
+                                        ,REDQ2
+                                        ,REDQ3
+                                        ,REDQ4
+                                        ,REDQ5)
+                                        VALUES
+                                        ( @Edicao  
+                                        , @AreaConhecimentoID
+                                        , @uad_sigla 
+                                        , REPLICATE('0', 6 - LEN(@esc_codigo)) + RTrim(@esc_codigo)
+                                        , @AnoEscolar
+                                        , @tur_codigo
+                                        , @tur_id
+                                        , @alu_matricula
+                                        , @alu_nome
+                                        , @NivelProficienciaID
+                                        , @Valor
+                                        , @REDQ1
+                                        , @REDQ2
+                                        , @REDQ3
+                                        , @REDQ4
+                                        , @REDQ5)";
 
                 var parametros = ObterParametros(resultado);
                 return await conn.ExecuteAsync(query, parametros);
-
             }
             catch (Exception ex)
             {
-                string log = $@"Inserir ResultadoAluno -- Erro:{ex.Message} ---- Objeto: {ResultadoPsp.ObterJsonObjetoResultado(resultado)}";
+                var log = $"Inserir ResultadoAluno -- Erro:{ex.Message} ---- Objeto: {ResultadoPsp.ObterJsonObjetoResultado(resultado)}";
                 throw new Exception(log);
             }
             finally
@@ -88,26 +102,30 @@ namespace SME.SERAp.Prova.Dados.Repositorios
             using var conn = ObterConexaoProvaSp();
             try
             {
-                var query = $@"update ResultadoAluno set							 
-                      	      uad_sigla = @uad_sigla
-                             ,esc_codigo = REPLICATE('0', 6 - LEN(@esc_codigo)) + RTrim(@esc_codigo)
-                             ,AnoEscolar = @AnoEscolar
-                             ,tur_codigo = @tur_codigo
-                             ,tur_id = @tur_id
-                             ,alu_nome = @alu_nome
-                             ,NivelProficienciaID = @NivelProficienciaID
-                             ,Valor = @Valor
-							 where alu_matricula = @alu_matricula
-								and AreaConhecimentoID = @AreaConhecimentoId
-                                and Edicao = @Edicao";
+                const string query = @"update ResultadoAluno set
+                                            uad_sigla = @uad_sigla
+                                            ,esc_codigo = REPLICATE('0', 6 - LEN(@esc_codigo)) + RTrim(@esc_codigo)
+                                            ,AnoEscolar = @AnoEscolar
+                                            ,tur_codigo = @tur_codigo
+                                            ,tur_id = @tur_id
+                                            ,alu_nome = @alu_nome
+                                            ,NivelProficienciaID = @NivelProficienciaID
+                                            ,Valor = @Valor
+                                            ,REDQ1 = @REDQ1
+                                            ,REDQ2 = @REDQ2
+                                            ,REDQ3 = @REDQ3
+                                            ,REDQ4 = @REDQ4
+                                            ,REDQ5 = @REDQ5
+                                        where alu_matricula = @alu_matricula
+                                        and AreaConhecimentoID = @AreaConhecimentoId
+                                        and Edicao = @Edicao";
 
                 var parametros = ObterParametros(resultado);
                 return await conn.ExecuteAsync(query, parametros);
-
             }
             catch (Exception ex)
             {
-                string log = $@"Alterar {resultado.GetType().Name} -- Erro:{ex.Message} ---- Objeto: {ResultadoPsp.ObterJsonObjetoResultado(resultado)}";
+                var log = $"Alterar {resultado.GetType().Name} -- Erro:{ex.Message} ---- Objeto: {ResultadoPsp.ObterJsonObjetoResultado(resultado)}";
                 throw new Exception(log);
             }
             finally
@@ -115,9 +133,9 @@ namespace SME.SERAp.Prova.Dados.Repositorios
                 conn.Close();
                 conn.Dispose();
             }
-
         }
-        private DynamicParameters ObterParametros(ResultadoAluno resultado)
+        
+        private static DynamicParameters ObterParametros(ResultadoAluno resultado)
         {
             var parametros = new DynamicParameters();
             parametros.Add("@Edicao", resultado.Edicao, DbType.String, ParameterDirection.Input, 10);
@@ -131,6 +149,11 @@ namespace SME.SERAp.Prova.Dados.Repositorios
             parametros.Add("@alu_nome", resultado.alu_nome, DbType.String, ParameterDirection.Input, 200);
             parametros.Add("@NivelProficienciaID", resultado.NivelProficienciaID, DbType.Int16, ParameterDirection.Input);
             parametros.Add("@Valor", resultado.Valor, DbType.Decimal, ParameterDirection.Input, null, 6, 3);
+            parametros.Add("@REDQ1", resultado.REDQ1, DbType.Decimal, ParameterDirection.Input, null, 6, 2);
+            parametros.Add("@REDQ2", resultado.REDQ2, DbType.Decimal, ParameterDirection.Input, null, 6, 2);
+            parametros.Add("@REDQ3", resultado.REDQ3, DbType.Decimal, ParameterDirection.Input, null, 6, 2);
+            parametros.Add("@REDQ4", resultado.REDQ4, DbType.Decimal, ParameterDirection.Input, null, 6, 2);
+            parametros.Add("@REDQ5", resultado.REDQ5, DbType.Decimal, ParameterDirection.Input, null, 6, 2);
             return parametros;
         }
     }

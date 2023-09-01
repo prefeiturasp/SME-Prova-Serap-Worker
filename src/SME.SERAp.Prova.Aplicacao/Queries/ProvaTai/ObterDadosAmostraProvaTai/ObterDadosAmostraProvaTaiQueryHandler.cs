a344710789a1f8627f57cfa105aa25ed45fd3997
fernando.groeler <fernando.groeler@gmail.com>
2023-09-01 13:44:50 -0400

    Implementado para contemplar o processamento de multiplas matrizes de avalição ao gerar os dados da amostra da prova.

--- src/SME.SERAp.Prova.Aplicacao/Queries/ProvaTai/ObterDadosAmostraProvaTai/ObterDadosAmostraProvaTaiQueryHandler.cs
+++ src/SME.SERAp.Prova.Aplicacao/Queries/ProvaTai/ObterDadosAmostraProvaTai/ObterDadosAmostraProvaTaiQueryHandler.cs
@@ -2,14 +2,14 @@
 using SME.SERAp.Prova.Dados;
 using SME.SERAp.Prova.Infra;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace SME.SERAp.Prova.Aplicacao
 {
-    public class ObterDadosAmostraProvaTaiQueryHandler : IRequestHandler<ObterDadosAmostraProvaTaiQuery, AmostraProvaTaiDto>
-    {
-        
+    public class ObterDadosAmostraProvaTaiQueryHandler : IRequestHandler<ObterDadosAmostraProvaTaiQuery, IEnumerable<AmostraProvaTaiDto>>
+    {   
         private readonly IRepositorioProvaLegado repositorioProvaLegado;
 
         public ObterDadosAmostraProvaTaiQueryHandler(IRepositorioProvaLegado repositorioProvaLegado)
@@ -17,7 +17,7 @@ namespace SME.SERAp.Prova.Aplicacao
             this.repositorioProvaLegado = repositorioProvaLegado ?? throw new ArgumentNullException(nameof(repositorioProvaLegado));
         }
 
-        public async Task<AmostraProvaTaiDto> Handle(ObterDadosAmostraProvaTaiQuery request, CancellationToken cancellationToken)
+        public async Task<IEnumerable<AmostraProvaTaiDto>> Handle(ObterDadosAmostraProvaTaiQuery request, CancellationToken cancellationToken)
         {
             return await repositorioProvaLegado.ObterDadosAmostraProvaTai(request.ProvaLegadoId);
         }

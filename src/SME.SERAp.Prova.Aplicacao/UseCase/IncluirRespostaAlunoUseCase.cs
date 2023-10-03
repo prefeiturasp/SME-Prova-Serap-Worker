b8221f2a80bddc38530593f71f7fca9d1c64bbdf
fernando.groeler <fernando.groeler@gmail.com>
2023-10-03 12:15:21 -0400

    Revert "Incluído tratamento para obter o erro ao incluir ou alterar a questão do aluno."
    This reverts commit 5bfdcc53972e567e3b1c1b1ae3b4ac66a2f15b35.

--- src/SME.SERAp.Prova.Aplicacao/UseCase/IncluirRespostaAlunoUseCase.cs
+++ src/SME.SERAp.Prova.Aplicacao/UseCase/IncluirRespostaAlunoUseCase.cs
@@ -2,7 +2,6 @@
 using SME.SERAp.Prova.Infra;
 using System;
 using System.Threading.Tasks;
-using SME.SERAp.Prova.Infra.Exceptions;
 
 namespace SME.SERAp.Prova.Aplicacao
 {
@@ -17,60 +16,47 @@ namespace SME.SERAp.Prova.Aplicacao
 
         public async Task<bool> Executar(MensagemRabbit mensagemRabbit)
         {
-            var isAlteracao = false;
+            var dto = mensagemRabbit.ObterObjetoMensagem<QuestaoAlunoRespostaSincronizarDto>();
+            var horaDataResposta = new DateTime(dto.DataHoraRespostaTicks);
             
-            try
+            horaDataResposta = horaDataResposta.AddHours(-3);
+
+            var questaoRespondida = await mediator.Send(new ObterQuestaoAlunoRespostaPorIdRaQuery(dto.QuestaoId, dto.AlunoRa));
+
+            if (questaoRespondida == null)
             {
-                var dto = mensagemRabbit.ObterObjetoMensagem<QuestaoAlunoRespostaSincronizarDto>();
-                
-                var horaDataResposta = new DateTime(dto.DataHoraRespostaTicks);
-            
-                horaDataResposta = horaDataResposta.AddHours(-3);
-
-                var questaoRespondida = await mediator.Send(new ObterQuestaoAlunoRespostaPorIdRaQuery(dto.QuestaoId, dto.AlunoRa));
-
-                if (questaoRespondida == null)
-                {
-                    return await mediator.Send(new IncluirQuestaoAlunoRespostaCommand(dto.QuestaoId,
-                        dto.AlunoRa,
-                        dto.AlternativaId,
-                        dto.Resposta,
-                        horaDataResposta,
-                        dto.TempoRespostaAluno ?? 0,
-                        dto.DispositivoId));
-                }
-
-                isAlteracao = true;
-
-                if (questaoRespondida.CriadoEm.Date > DateTime.Now.Date)
-                    questaoRespondida.CriadoEm = DateTime.Now.Date;
-
-                if (questaoRespondida.AlternativaId != null)
-                {
-                    if (questaoRespondida.CriadoEm > horaDataResposta)
-                        return false;
-                }
-
-                if (questaoRespondida.AlternativaId == null && !string.IsNullOrEmpty(questaoRespondida.Resposta))
-                {
-                    if (questaoRespondida.CriadoEm > horaDataResposta)
-                        return false;
-                }
-
-                questaoRespondida.Resposta = dto.Resposta;
-                questaoRespondida.AlternativaId = dto.AlternativaId;
-                questaoRespondida.TempoRespostaAluno += dto.TempoRespostaAluno ?? 0;
-                questaoRespondida.CriadoEm = horaDataResposta;
-                questaoRespondida.Visualizacoes += 1;
-                questaoRespondida.DispositivoId = dto.DispositivoId;
-            
-                return await mediator.Send(new AtualizarQuestaoAlunoRespostaCommand(questaoRespondida));                
+                return await mediator.Send(new IncluirQuestaoAlunoRespostaCommand(dto.QuestaoId,
+                    dto.AlunoRa,
+                    dto.AlternativaId,
+                    dto.Resposta,
+                    horaDataResposta,
+                    dto.TempoRespostaAluno ?? 0,
+                    dto.DispositivoId));
             }
-            catch (Exception e)
+
+            if (questaoRespondida.CriadoEm.Date > DateTime.Now.Date)
+                questaoRespondida.CriadoEm = DateTime.Now.Date;
+
+            if (questaoRespondida.AlternativaId != null)
             {
-                var msgIncluirOuAlterar = isAlteracao ? "alterar" : "incluir";
-                throw new ErroException($"Erro ao {msgIncluirOuAlterar} resposta do aluno: {e.Message}");
+                if (questaoRespondida.CriadoEm > horaDataResposta)
+                    return false;
             }
+
+            if (questaoRespondida.AlternativaId == null && !string.IsNullOrEmpty(questaoRespondida.Resposta))
+            {
+                if (questaoRespondida.CriadoEm > horaDataResposta)
+                    return false;
+            }
+
+            questaoRespondida.Resposta = dto.Resposta;
+            questaoRespondida.AlternativaId = dto.AlternativaId;
+            questaoRespondida.TempoRespostaAluno += dto.TempoRespostaAluno ?? 0;
+            questaoRespondida.CriadoEm = horaDataResposta;
+            questaoRespondida.Visualizacoes += 1;
+            questaoRespondida.DispositivoId = dto.DispositivoId;
+
+            return await mediator.Send(new AtualizarQuestaoAlunoRespostaCommand(questaoRespondida));
         }
     }
 }

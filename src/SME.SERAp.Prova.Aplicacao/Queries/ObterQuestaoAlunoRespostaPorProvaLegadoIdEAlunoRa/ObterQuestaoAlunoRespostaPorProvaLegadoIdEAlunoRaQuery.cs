ccc30a828af78c70028fb4437f6c8e39a99627d7
fernando.groeler <fernando.groeler@gmail.com>
2024-04-05 15:13:37 -0400

    Correção da consulta para prova bib.

--- src/SME.SERAp.Prova.Aplicacao/Queries/ObterQuestaoAlunoRespostaPorProvaLegadoIdEAlunoRa/ObterQuestaoAlunoRespostaPorProvaLegadoIdEAlunoRaQuery.cs
+++ src/SME.SERAp.Prova.Aplicacao/Queries/ObterQuestaoAlunoRespostaPorProvaLegadoIdEAlunoRa/ObterQuestaoAlunoRespostaPorProvaLegadoIdEAlunoRaQuery.cs
@@ -6,13 +6,15 @@ namespace SME.SERAp.Prova.Aplicacao
 {
     public class ObterQuestaoAlunoRespostaPorProvaLegadoIdEAlunoRaQuery : IRequest<IEnumerable<AlunoQuestaoRespostasDto>>
     {
-        public ObterQuestaoAlunoRespostaPorProvaLegadoIdEAlunoRaQuery(long provaLegadoId, long alunoRa)
+        public ObterQuestaoAlunoRespostaPorProvaLegadoIdEAlunoRaQuery(long provaLegadoId, long alunoRa, bool possuiBib)
         {
             ProvaLegadoId = provaLegadoId;
             AlunoRa = alunoRa;
+            PossuiBib = possuiBib;
         }
 
         public long ProvaLegadoId { get; }
         public long AlunoRa { get; }
+        public bool PossuiBib { get; }
     }
 }

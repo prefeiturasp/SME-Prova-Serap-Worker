0b66dcd4a854ffb2aeee3aec56e879eede8c27db
Mario Junior <mariosj9@gmail.com>
2025-08-05 13:39:15 -0300

    AB#131827

---
+++ src/SME.SERAp.Prova.Aplicacao/Commands/QuestaoAlunoAdministrado/ExcluirQuestaoAlunoTai/ExcluirQuestaoAlunoTaiCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using SME.SERAp.Prova.Dados;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.SERAp.Prova.Aplicacao.Commands
+{
+    public class ExcluirQuestaoAlunoTaiCommandHandler : IRequestHandler<ExcluirQuestaoAlunoTaiCommand, int>
+    {
+        private readonly IRepositorioQuestaoAlunoTai repositorioQuestaoAlunoTai;
+        public ExcluirQuestaoAlunoTaiCommandHandler(IRepositorioQuestaoAlunoTai repositorioQuestaoAlunoTai)
+        {
+            this.repositorioQuestaoAlunoTai = repositorioQuestaoAlunoTai;
+        }
+        public async Task<int> Handle(ExcluirQuestaoAlunoTaiCommand request, CancellationToken cancellationToken)
+        {
+            return await repositorioQuestaoAlunoTai.ExcluirQuestaoAlunoTai(request.ProvaId, request.AlunoRa);
+        }
+    }
+}

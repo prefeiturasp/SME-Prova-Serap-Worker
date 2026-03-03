db5231ac3893c90ba2bf5f46babcb303d757a25d
Mario Junior <mariosj9@gmail.com>
2026-03-03 11:34:14 -0300

    AB#144154

--- src/SME.SERAp.Prova.Dados/Repositorios/RepositorioBaseEntity.cs
+++ src/SME.SERAp.Prova.Dados/Repositorios/RepositorioBaseEntity.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Npgsql;
 using Npgsql.Bulk;
 using SME.SERAp.Prova.Dominio;
 using SME.SERAp.Prova.Infra.EnvironmentVariables;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SME.SERAp.Prova.Dados
@@ -17,27 +19,56 @@ namespace SME.SERAp.Prova.Dados
             this.connectionStrings = connectionStrings ?? throw new ArgumentNullException(nameof(connectionStrings));
         }
 
+        private static void MapearEnumsAutomaticamente(NpgsqlDataSourceBuilder builder)
+        {
+            var mapEnumMethod = typeof(NpgsqlDataSourceBuilder)
+                .GetMethods()
+                .First(m => m.Name == nameof(NpgsqlDataSourceBuilder.MapEnum)
+                            && m.GetParameters().Length == 2
+                            && m.GetParameters()[0].ParameterType == typeof(string)
+                            && m.GetParameters()[1].ParameterType == typeof(INpgsqlNameTranslator));
+
+            var assemblies = new[]
+            {
+                typeof(EntidadeBase).Assembly,   // Dominio
+            };
+
+            var enumTypes = assemblies
+                .SelectMany(a => a.GetTypes())
+                .Where(t => t.IsEnum)
+                .Distinct();
+
+            foreach (var enumType in enumTypes)
+            {
+                mapEnumMethod!
+                    .MakeGenericMethod(enumType)
+                    .Invoke(builder, [null, null]);
+            }
+        }
+
+        private ContextoDbSerap CriarContexto()
+        {
+            var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionStrings.ApiSerap);
+            MapearEnumsAutomaticamente(dataSourceBuilder);
+            var dataSource = dataSourceBuilder.Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<ContextoDbSerap>();
+            optionsBuilder.UseNpgsql(dataSource);
+
+            return new ContextoDbSerap(optionsBuilder.Options);
+        }
+
         public async Task InserirVariosAsync(IEnumerable<T> entidades)
         {
-            var optionsBuilder = new DbContextOptionsBuilder<ContextoDbSerap>();
-            optionsBuilder.UseNpgsql(connectionStrings.ApiSerap);
-
-            await using var dbContext = new ContextoDbSerap(optionsBuilder.Options);
-
+            await using var dbContext = CriarContexto();
             var uploader = new NpgsqlBulkUploader(dbContext);
-
             await uploader.InsertAsync(entidades);
         }
 
         public async Task AlterarVariosAsync(IEnumerable<T> entidades)
         {
-            var optionsBuilder = new DbContextOptionsBuilder<ContextoDbSerap>();
-            optionsBuilder.UseNpgsql(connectionStrings.ApiSerap);
-
-            await using var dbContext = new ContextoDbSerap(optionsBuilder.Options);
-
+            await using var dbContext = CriarContexto();
             var uploader = new NpgsqlBulkUploader(dbContext);
-
             await uploader.UpdateAsync(entidades);
         }
     }
